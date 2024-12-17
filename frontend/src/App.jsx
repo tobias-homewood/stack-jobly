6fb6e72d8917f176eae83c5d9e033111dc4a3612
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-12-17 18:47:53 +0000

    app routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,26 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
+// import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [currentUser, setCurrentUser] = useState({username: 'testUser'})
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <nav>Navbar Goes Here</nav>
+      <Routes>
+        <Route index element={<div>This is the HOMEPAGE</div>} />
+        <Route path="/companies">
+          <Route index element={<div>This is the COMPANIES LIST</div>} />
+          <Route path=":handle" element={<div>This is the COMPANY JOB LIST</div>} />
+        </Route>
+        <Route path="/jobs" element={<div>This is the JOBS LIST</div>} />
+        <Route path="/profile" element={<div>This is the PROFILE PAGE</div>} />
+        <Route path="/login" element={<div>This is the LOGIN PAGE</div>} />
+        <Route path="/signup" element={<div>This is the SIGNUP PAGE</div>} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   )
 }
 
