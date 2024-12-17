bc3b6f6fdc89d387efe33a8dc195d5a023f921f6
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-12-17 18:53:43 +0000

    refactor routing logic to conditionally render routes based on user authentication

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,60 @@
-import { useState } from 'react'
-import { BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom'
-// import './App.css'
+import { useState } from "react";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+import './App.css'
 
 function App() {
-  const [currentUser, setCurrentUser] = useState({username: 'testUser'})
+    const [currentUser, setCurrentUser] = useState({ username: "testUser" });
 
-  return (
-    <Router>
-      <nav>Navbar Goes Here</nav>
-      <Routes>
-        <Route index element={<div>This is the HOMEPAGE</div>} />
-        <Route path="/companies">
-          <Route index element={<div>This is the COMPANIES LIST</div>} />
-          <Route path=":handle" element={<div>This is the COMPANY JOB LIST</div>} />
-        </Route>
-        <Route path="/jobs" element={<div>This is the JOBS LIST</div>} />
-        <Route path="/profile" element={<div>This is the PROFILE PAGE</div>} />
-        <Route path="/login" element={<div>This is the LOGIN PAGE</div>} />
-        <Route path="/signup" element={<div>This is the SIGNUP PAGE</div>} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
-  )
+    return (
+        <Router>
+            <nav>Navbar Goes Here</nav>
+            <Routes>
+                <Route index element={<div>This is the HOMEPAGE</div>} />
+
+                {currentUser ? (
+                    <>
+                        <Route path="/companies">
+                            <Route
+                                index
+                                element={<div>This is the COMPANIES LIST</div>}
+                            />
+                            <Route
+                                path=":handle"
+                                element={
+                                    <div>This is the COMPANY JOB LIST</div>
+                                }
+                            />
+                        </Route>
+                        <Route
+                            path="/jobs"
+                            element={<div>This is the JOBS LIST</div>}
+                        />
+                        <Route
+                            path="/profile"
+                            element={<div>This is the PROFILE PAGE</div>}
+                        />
+                    </>
+                ) : (
+                    <>
+                        <Route
+                            path="/login"
+                            element={<div>This is the LOGIN PAGE</div>}
+                        />
+                        <Route
+                            path="/signup"
+                            element={<div>This is the SIGNUP PAGE</div>}
+                        />
+                    </>
+                )}
+                <Route path="*" element={<Navigate to="/" />} />
+            </Routes>
+        </Router>
+    );
 }
 
-export default App
+export default App;
