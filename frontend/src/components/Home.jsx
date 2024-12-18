8ba4e0035a75a7a804a26c46592c23ee229dcf64
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-12-18 18:28:17 +0000

    home page with react bootstrap

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,17 +1,18 @@
 import { useContext } from 'react';
 import CurrentUserContext from '../utils/currentUserContext';
 import { Link } from 'react-router-dom';
+import { Button } from 'react-bootstrap';
 
 const Home = () => {
   const { currentUser } = useContext(CurrentUserContext);
   return (
-    <div>
+    <div className='mt-5 text-center text-light'>
       <h1>Welcome to Jobly</h1>
       <p>The best place to find your next job!</p>
       {currentUser ? <h2>Welcome back, {currentUser.username}!</h2> : (
         <div>
-          <Link to="/login"><button>Log in</button></Link>
-          <Link to="/signup"><button>Sign up</button></Link>
+          <Button as={Link} to='/login' className='m-2'>Log in</Button>
+          <Button as={Link} to='/signup' className='m-2'>Sign up</Button>
         </div>
       )}
     </div>
