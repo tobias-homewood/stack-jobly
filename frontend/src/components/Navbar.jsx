f8412ea80cfe54d8fc02e5e5bb9568fcb7afa4cd
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-12-18 17:52:25 +0000

    navbar uses bootstrap components

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,34 +1,51 @@
 import { useContext } from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
 import { Link, NavLink, useNavigate } from "react-router-dom";
 import CurrentUserContext from "../utils/currentUserContext";
 
-const Navbar = () => {
+export default function () {
     const navigate = useNavigate();
     const { currentUser, setCurrentUser } = useContext(CurrentUserContext);
     const logout = (e) => {
         e.preventDefault();
         setCurrentUser(null);
         navigate("/");
-    }
+    };
     return (
-        <nav>
-            <NavLink to="/">Jobly</NavLink>
-
-            {currentUser ? (
-                <>
-                    <NavLink to="/companies">Companies</NavLink>
-                    <NavLink to="/jobs">Jobs</NavLink>
-                    <NavLink to="/profile">Profile</NavLink>
-                    <Link onClick={logout}>Logout</Link>
-                </>
-            ) : (
-                <>
-                    <NavLink to="/login">Login</NavLink>
-                    <NavLink to="/signup">Signup</NavLink>
-                </>
-            )}
-        </nav>
+        <Navbar expand="lg" className="bg-body-tertiary">
+            <Container>
+                <Navbar.Brand as={Link} to="/">
+                    Jobly
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="ms-auto">
+                        {currentUser ? (
+                            <>
+                                <Nav.Link as={NavLink} to="/companies">
+                                    Companies
+                                </Nav.Link>
+                                <Nav.Link as={NavLink} to="/jobs">
+                                    Jobs
+                                </Nav.Link>
+                                <Nav.Link as={NavLink} to="/profile">
+                                    Profile
+                                </Nav.Link>
+                                <Nav.Link onClick={logout}>Logout</Nav.Link>
+                            </>
+                        ) : (
+                            <>
+                                <Nav.Link as={NavLink} to="/login">
+                                    Login
+                                </Nav.Link>
+                                <Nav.Link as={NavLink} to="/signup">
+                                    Signup
+                                </Nav.Link>
+                            </>
+                        )}
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
     );
-};
-
-export default Navbar;
+}
