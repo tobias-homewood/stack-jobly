206454232431daf4659a5db99c448c8ea3f39cd0
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-12-19 16:27:56 +0000

    implement login functionality with form handling and navigation

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,20 +1,33 @@
-import { useContext } from 'react'
-import CurrentUserContext from '../utils/currentUserContext'
+import { useContext } from "react";
+import CurrentUserContext from "../utils/currentUserContext";
+import { useNavigate } from "react-router-dom";
+import JoblyApi from "../utils/api";
+import Form from "./Form";
 
 const Login = () => {
     const { setCurrentUser } = useContext(CurrentUserContext);
-    const bypassLogin = () => {
-        setCurrentUser({ username: "logedInAgain" })
-    }
 
-  return (
-    <div>
-        <h1>Log In</h1>
-        <div>THIS IS WHERE THE FORM SHOULD BE</div>
-        {/* this is while  authentication is not ready */}
-        <button onClick={bypassLogin}><h1>Bypass Login</h1></button>
-    </div>
-  )
-}
+    const navigate = useNavigate();
 
-export default Login
+    const login = async (formData) => {
+        const token = await JoblyApi.login(formData);
+        JoblyApi.token = token;
+        const user = await JoblyApi.getUser(formData.username);
+        setCurrentUser(user);
+        navigate("/");
+    };
+
+    return (
+        <Form
+            title="Log In"
+            inputs={[
+                { name: "username", label: "Username" },
+                { name: "password", label: "Password", type: "password" },
+            ]}
+            onSubmit={login}
+            submitButtonText="Log in"
+        />
+    );
+};
+
+export default Login;
