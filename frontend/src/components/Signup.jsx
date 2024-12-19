1bacc3f20e818da91875866cfd2d81f932ff9e83
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-12-19 16:42:56 +0000

    implement signup functionality with form handling and user context integration

--- frontend/src/components/Signup.jsx
+++ frontend/src/components/Signup.jsx
@@ -1,12 +1,38 @@
-import React from 'react'
+import { useContext } from "react";
+import CurrentUserContext from "../utils/currentUserContext";
+import { useNavigate } from "react-router-dom";
+import JoblyApi from "../utils/api";
+import Form from "./Form";
 
 const Signup = () => {
-  return (
-    <div>
-        <h1>Sign Up</h1>
-        <div>THIS IS WHERE THE FORM SHOULD BE</div>
-    </div>
-  )
-}
+    const { setCurrentUser } = useContext(CurrentUserContext);
 
-export default Signup
+    const navigate = useNavigate();
+
+    const signup = async (formData) => {
+        const token = await JoblyApi.register(formData);
+        JoblyApi.token = token;
+        const user = await JoblyApi.getUser(formData.username);
+        setCurrentUser(user);
+        navigate("/");
+    }
+
+    return (
+        <div>
+            <Form
+                title="Sign Up"
+                inputs={[
+                    { name: "username", label: "Username" },
+                    { name: "firstName", label: "First Name" },
+                    { name: "lastName", label: "Last Name" },
+                    { name: "email", label: "Email", type: "email" },
+                    { name: "password", label: "Password", type: "password" },
+                ]}
+                onSubmit={signup}
+                submitButtonText="Sign up"
+            />
+        </div>
+    );
+};
+
+export default Signup;
