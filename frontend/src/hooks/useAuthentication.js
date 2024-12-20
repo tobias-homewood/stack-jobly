683eaa254f7021902b0a91dd221559f1ea5ab03d
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-12-20 18:04:10 +0000

    fix authentication hook: added missing token to the api at beginning

--- frontend/src/hooks/useAuthentication.js
+++ frontend/src/hooks/useAuthentication.js
@@ -4,18 +4,24 @@ import useLoading from "./useLoading";
 import JoblyApi from "../utils/api";
 
 export default function useAuthentication() {
+    // Get the token from local storage
     const [token, setToken] = useLocalStorage("token");
+    // Set the token in the JoblyApi
+    JoblyApi.token = token;
 
+    // Every time the token changes, update the token in the JoblyApi
     const saveToken = (userToken) => {
         JoblyApi.token = userToken;
         setToken(userToken);
     };
 
+    // Clear the token from local storage and the JoblyApi
     const clearToken = () => {
         JoblyApi.token = null;
         setToken(null);
     };
 
+    // Get the user from the JoblyApi
     const getUser = async () => {
         try {
             const { username } = JoblyApi.decode(token);
@@ -25,8 +31,10 @@ export default function useAuthentication() {
         }
     };
 
+    // Get the user from the JoblyApi and set it in state
     const [currentUser, setCurrentUser, isLoading] = useLoading(getUser());
 
+    // Get the user from the JoblyApi when the token changes
     useEffect(() => {
         setCurrentUser(getUser());
     }, [token]);
