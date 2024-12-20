aa0e709a0e225a74f23d8fedc9d73a8c292e0b62
tobias-homewood <toby_homewood@hotmail.co.uk>
2024-12-20 18:16:06 +0000

    company details ready with list of jobs

--- frontend/src/components/CompanyDetail.jsx
+++ frontend/src/components/CompanyDetail.jsx
@@ -1,8 +1,27 @@
 import { useParams } from "react-router-dom";
+import JoblyApi from "../utils/api";
+import useLoading from "../hooks/useLoading";
+import { Spinner, Container } from "react-bootstrap";
+import Job from "./Job";
 
 const CompanyDetail = () => {
     const { handle } = useParams();
-    return <div>Company Details for: {handle}</div>;
+    const [company, _, isLoading] = useLoading(
+        JoblyApi.getCompany(handle)
+    );
+
+    if (isLoading) {
+        return <Spinner animation="border" />;
+    }
+    return (
+        <Container>
+            <h1 className="text-light mt-3">{company.name}</h1>
+            <p className="text-light">{company.description}</p>
+            {company.jobs.map((job) => (
+                <Job key={job.id} job={job} />
+            ))}
+        </Container>
+    );
 };
 
 export default CompanyDetail;
